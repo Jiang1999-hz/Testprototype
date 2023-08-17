98236fb382ec6ae3f34687e011115373c75983a1
Jiang1999-hz <303314107jcr@gmail.com>
2023-08-17 23:37:02 +0900

    first try

---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnIntervalUpLimit = 5;
+    private float spawnIntervalDownLimit = 3;
+    public int ballIndex = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, Random.Range(spawnIntervalDownLimit,spawnIntervalUpLimit));
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[Random.Range(0,ballIndex)], spawnPos, ballPrefabs[Random.Range(0,ballIndex)].transform.rotation);
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    public float spawnRangeX  = 20;
+    public float spawnPosZ  = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal",startDelay,spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+    void SpawnRandomAnimal(){
+            int animalIndex = Random.Range(0,animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex],new Vector3(Random.Range(-spawnRangeX,spawnRangeX),0,spawnPosZ),animalPrefabs[animalIndex].transform.rotation);
+        }
+}
